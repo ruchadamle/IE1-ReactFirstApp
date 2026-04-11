a16e5dc0b9b868f93f0ac9b7fe130c1edd2c6ee2
Rucha Damle <ruchadamle@email.com>
2026-04-10 17:23:44 -0700

    feat: add functionality add user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -54,6 +54,17 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
 app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name !== undefined) {
