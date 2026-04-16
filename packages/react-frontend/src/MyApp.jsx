af079c434b7764e44058bb54714d20445bb4baa7
Rucha Damle <ruchadamle@email.com>
2026-04-16 16:55:55 -0700

    fix: typo in URL

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -45,7 +45,7 @@ function MyApp() {
     }
 
     function postUser(person) {
-        const promise = fetch("Http://localhost:8000/users", {
+        const promise = fetch("http://localhost:8000/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
